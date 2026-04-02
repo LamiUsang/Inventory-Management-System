e1d84f2de4d58f03310db42d7feaf1e7026948cd
Olamide Adeboye <mailolamide@gmail.com>
2026-04-02 05:37:07 +0100

    Revert "Merge pull request #1 from LamiUsang/feature/products"
    This reverts commit 2b970dda5c8c480583973838a21f5b93f7d69b0f, reversing
    changes made to 58aef49d1f72f406e5a363de8205bffb03afe574.

--- App/products.py
+++
@@ -1,9 +0,0 @@
-def _init_(
-    self, product_id, name, unit_quantity, unit_price, stock_quantity, shelf_life
-):
-    self.product_id = product_id
-    self.name = name
-    self.unit_quantity = unit_quantity
-    self.unit_price = unit_price
-    self.stock_quantity = stock_quantity
-    self.shelf_life = shelf_life
