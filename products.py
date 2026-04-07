82b0c1b0691905c37f8ef91bbbd7e59ef925bfc9
Alexanoscan <alexomeje@gmail.com>
2026-04-07 12:45:54 +0100

    Update products.py
    1. I created the parent product class "class product" with the major attributes of all products both perishable and digital; product id, name, price, quantity, and category.
    
    2. for the perishable products; im001 - im005, I defined a sub-class "class Perishable Product", using the "super ()" I was able to call on the parent class attributes without having to define all the attributes again in the perishable product class. only defined the only added attribute "expiry date".
    
    3. I created the objects product_im001 - product_im005 from this perishable product sub-class.
    
    4. I created another sub-class "Digital Product", using the "sup ()" function to call in the attributes from the parent class "product"
    
    5. I defined the digital product sub-class attributes with quantity = 0 and added another attribute "download link".
    
    6. I created the last item on the list product_im006 under the digital product sub-class with its attributes including quantity = 0 and download link.

--- products.py
+++ products.py
@@ -11,9 +11,13 @@ logger = logging.getLogger("InventoryManager")
 
 
 class Product:
-    """Base class for all products."""
-
-    def __init__(self, product_id, name, price, quantity, category):
+       def __init__(self, product_id, name, price, quantity, category):
+           self.product_id = product_id
+           self.name = name
+           self.price = price
+           self.quantity = quantity
+           self.category = category
+           
         """
         Raises:
             InvalidProductError: If any validation fails.
@@ -56,10 +60,54 @@ class Product:
 
 
 class PerishableProduct(Product):
-    """A product with an expiry date."""
-
-    def __init__(self, product_id, name, price, quantity, category, expiry_date):
+     def __init__(self, product_id, name, price, quantity, category, expiry_date):
         super().__init__(product_id, name, price, quantity, category)
+         self.expiry_date = expiry date
+
+product_im001 = PerishableProduct(
+    product_id="IM001",
+    name="Milk",
+    price=16.55,
+    quantity=200,
+    category="Dairy",
+    shelf_life="3 months"
+)
+
+product_im002 = PerishableProduct(
+    product_id="IM002",
+    name="Bread",
+    price=2.99,
+    quantity=100,
+    category="Gluten Free",
+    shelf_life="2 weeks"
+)
+
+product_im003 = PerishableProduct(
+    product_id="IM003",
+    name="Sugar",
+    price=1.20,
+    quantity=200,
+    category="Confectionary",
+    shelf_life="6 months"
+)
+
+product_im004 = PerishableProduct(
+    product_id="IM004",
+    name="Egg",
+    price=6.5,
+    quantity=50,
+    category="Free Range",
+    shelf_life="2 Weeks"
+)
+
+product_im005 = PerishableProduct(
+    product_id="IM005",
+    name="Butter",
+    price=5.0,
+    quantity=100,
+    category="Plant Based",
+    shelf_life="6 months"
+)
         # TODO: Validate expiry_date format (YYYY-MM-DD).
         # TODO: Assign self.expiry_date.
         pass
@@ -81,10 +129,17 @@ class PerishableProduct(Product):
 
 
 class DigitalProduct(Product):
-    """A digital product with a download link (unlimited supply)."""
-
-    def __init__(self, product_id, name, price, category, download_link):
+     def __init__(self, product_id, name, price, category, download_link):
         super().__init__(product_id, name, price, 0, category)
+product_im006 = DigitalProduct(
+    product_id="IM006",
+    name="Python ebook",
+    price=5.99,
+    quantity=0
+    category="ebook",
+    download_link="download_link"
+)
+
         # TODO: Validate download_link non-empty.
         pass
 
